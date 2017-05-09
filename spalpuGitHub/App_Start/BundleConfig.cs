c183bd87fcb796902d701f659259982574b252c1
Yuriy <georgheim@mail.ru>
2017-05-09 21:33:24 +0600

    Сделал Tree

--- spalpuGitHub/App_Start/BundleConfig.cs
+++ spalpuGitHub/App_Start/BundleConfig.cs
@@ -9,7 +9,10 @@ namespace spalpuGitHub
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/bootstrap.js",
+                        "~/Scripts/respond.js",
+                        "~/Scripts/metisMenu.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
