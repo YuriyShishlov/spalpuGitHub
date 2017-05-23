1a5b82765865848f5117909a477db2606db3dddb
Yuriy <georgheim@mail.ru>
2017-05-24 00:02:34 +0600

    Регистрация заработала в черновом варианте

---
+++ spalpuGitHub/Controllers/AccountController.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using spalpuGitHub.Models;
+
+namespace spalpuGitHub.Controllers
+{
+    public class AccountController : Controller
+    {
+        public ActionResult Action()
+        {
+            return View();
+        }
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(UserAccount model)
+        {
+            if (ModelState.IsValid)
+            {
+                UserAccount user = null;
+                using (UserDbContext db = new UserDbContext())
+                {
+                    user = db.UserAccounts.FirstOrDefault(u => u.Email == model.Email);
+
+                }
+                if (user == null)
+                {
+                    // создаем нового пользователя
+                    using (UserDbContext db = new UserDbContext())
+                    {
+                        db.UserAccounts.Add(new UserAccount() { Id = Guid.NewGuid().ToString(), UserName = model.UserName, Password = model.Password, Email = model.Email, ConfirmEmail = false, Tasks = null });
+                        db.SaveChanges();
+
+                        user = db.UserAccounts.Where(u => u.Email == model.Email && u.Password == model.Password).FirstOrDefault();
+                    }
+                    // если пользователь удачно добавлен в бд
+                    if (user != null)
+                    {
+                        FormsAuthentication.SetAuthCookie(model.UserName, true);
+                        return RedirectToAction("Index", "Home");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("", "Пользователя с таким логином и паролем нет");
+                    }
+                }
+
+            }
+            return View(model);
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(UserAccount model)
+        {
+            if (ModelState.IsValid)
+            {
+                // поиск пользователя в бд
+                UserAccount user = null;
+                using (UserDbContext db = new UserDbContext())
+                {
+                    user = db.UserAccounts.FirstOrDefault(u => u.Email == model.Email && u.Password == model.Password);
+
+                }
+                if (user != null)
+                {
+                    FormsAuthentication.SetAuthCookie(model.UserName, true);
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Пользователя с таким логином и паролем нет");
+                }
+            }
+
+            return View(model);
+        }
+
+        
+
+        public ActionResult Logoff()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ spalpuGitHub/Models/AccountModels.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+
+namespace spalpuGitHub.Models
+{
+    public class AccountModels
+    {
+    }
+    public class UserDbContext : DbContext
+    {
+        public UserDbContext()
+            : base("name = UserConnectionString")
+        {
+        }
+        public virtual DbSet<UserAccount> UserAccounts { get; set; }
+        public virtual DbSet<Task> Tasks { get; set; }
+    }
+
+    public class UserAccount
+    {
+        [Key]
+        public string Id { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public bool ConfirmEmail { get; set; }
+        public ICollection<Task> Tasks { get; set; }
+    }
+
+    public class Task
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsCompleted { get; set; }
+        public UserAccount Owner { get; set; }
+    }
+
+    public class ResisterLoginModel
+    {
+        [Required(ErrorMessage = "Требуется указать отображаемое имя")]
+        [Display(Name = "Отображаемое имя")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Требуется указать email")]
+        [RegularExpression(@"^([\w-\.]+)@((\[[0-9]{1,3]\.)|(([\w-]+\.)+))([a-zA-Z{2,4}|[0-9]{1,3})(\]?)$", ErrorMessage = "Введите действительный адрес электронной почты")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Требуется указать пароль")]
+        public string Password { get; set; }
+
+        [Compare("Password", ErrorMessage = "Требуется подтвердить пароль")]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+
+    }
+}
